743b65e44472862b22638ddb1671510c88667253
jianong <jianonghe@gmail.com>
2020-10-05 19:39:58 +0800

    判断出胜者

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
+    if (calculateWinner(this.state.squares) || this.state.squares[i]) return
+
     const squares = this.state.squares.slice()
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
@@ -36,7 +38,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+
+    const status = winner
+      ? `Winner: ${winner}`
+      : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
 
     return (
       <div>
@@ -75,6 +81,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
